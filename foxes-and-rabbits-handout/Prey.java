c4f418d041c42acdcfe75bfd1d31a6f6474c9573
Laurence Andrews <hello@laurenceandrews.com>
2021-02-12 11:31:32 +0000

    Fixing GitHub Issues

--- foxes-and-rabbits-handout/Prey.java
+++ foxes-and-rabbits-handout/Prey.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -8,25 +9,14 @@ import java.util.Random;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
  */
-public class Rabbit extends Animal
+public abstract class Prey extends Animal
 {
-    // Characteristics shared by all rabbits (class variables).
-
-    // The age at which a rabbit can start to breed.
-    private static final int BREEDING_AGE = 5;
-    // The age to which a rabbit can live.
-    private static final int MAX_AGE = 40;
-    // The likelihood of a rabbit breeding.
-    private static final double BREEDING_PROBABILITY = 0.12;
-    // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 4;
-    // A shared random number generator to control breeding.
+    // 
     private static final Random rand = Randomizer.getRandom();
     
-    // Individual characteristics (instance fields).
-    
-    // The rabbit's age.
+    private int fear;
     private int age;
+    private int foodLevel;
 
     /**
      * Create a new rabbit. A rabbit may be created with age
@@ -36,15 +26,18 @@ public class Rabbit extends Animal
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
-    public Rabbit(boolean randomAge, Field field, Location location)
+    public Prey(boolean randomAge, Field field, Location location)
     {
         super(field, location);
-        age = 0;
         if(randomAge) {
-            age = rand.nextInt(MAX_AGE);
+            age = rand.nextInt(getMaxAge());
         }
+        else {
+            age = 0;
+        }
+        fear = 0;
     }
-    
+
     /**
      * This is what the rabbit does most of the time - it runs 
      * around. Sometimes it will breed or die of old age.
@@ -74,40 +67,23 @@ public class Rabbit extends Animal
     private void incrementAge()
     {
         age++;
-        if(age > MAX_AGE) {
+        if(age > getMaxAge()) {
             setDead();
         }
     }
-    
-    /**
-     * Check whether or not this rabbit is to give birth at this step.
-     * New births will be made into free adjacent locations.
-     * @param newRabbits A list to return newly born rabbits.
-     */
-    private void giveBirth(List<Animal> newRabbits)
-    {
-        // New rabbits are born into adjacent locations.
-        // Get a list of adjacent free locations.
-        Field field = getField();
-        List<Location> free = field.getFreeAdjacentLocations(getLocation());
-        int births = breed();
-        for(int b = 0; b < births && free.size() > 0; b++) {
-            Location loc = free.remove(0);
-            Rabbit young = new Rabbit(false, field, loc);
-            newRabbits.add(young);
-        }
-    }
-        
+
+    abstract void giveBirth(List<Animal> newPrey);
+
     /**
      * Generate a number representing the number of births,
      * if it can breed.
      * @return The number of births (may be zero).
      */
-    private int breed()
+    protected int breed()
     {
         int births = 0;
-        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
-            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        if(canBreed() && rand.nextDouble() <= getBreedingProbability()) {
+            births = rand.nextInt(getMaxLitterSize()) + 1;
         }
         return births;
     }
@@ -118,6 +94,32 @@ public class Rabbit extends Animal
      */
     private boolean canBreed()
     {
-        return age >= BREEDING_AGE;
+        return age >= getBreedingAge();
     }
+
+    private void increaseFear()
+    {
+        if (predatorsNearby().size() > 2) {
+            fear++;
+        }
+    }
+
+    /**
+     * A rabbit can breed if it has reached the breeding age.
+     * @return true if the rabbit can breed, false otherwise.
+     */
+    private boolean isScared()
+    {
+        return fear > 2;
+    }
+
+    // run twice as fast if scared
+
+    abstract int getBreedingAge();
+
+    abstract int getMaxAge();
+
+    abstract double getBreedingProbability();
+
+    abstract int getMaxLitterSize();
 }
